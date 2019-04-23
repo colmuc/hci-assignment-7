94beafb80019c479a4a5cc58a6fddf4173261524
Collins Mucyo <collinsmucyo@gmail.com>
2019-04-23 08:51:19 -0500

    Improved functionality

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const electron = require("electron");
-const url = require("url");
 const path = require("path");
+const url = require("url");
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu, ipcMain } = electron;
 
 let mainWindow;
+let addWindow;
 
 app.on("ready", function() {
   mainWindow = new BrowserWindow({});
-
   mainWindow.loadURL(
     url.format({
       pathname: path.join(__dirname, "mainWindow.html"),
@@ -16,4 +16,58 @@ app.on("ready", function() {
       slashes: true
     })
   );
+  mainWindow.on("closed", function() {
+    app.quit();
+  });
+
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+  Menu.setApplicationMenu(mainMenu);
 });
+
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: "Add Todo List Item"
+  });
+  addWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "addWindow.html"),
+      protocol: "file:",
+      slashes: true
+    })
+  );
+}
+
+ipcMain.on("item:add", function(e, item) {
+  mainWindow.webContents.send("item:add", item);
+  addWindow.close();
+});
+
+const mainMenuTemplate = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Add Item",
+        click() {
+          createAddWindow();
+        }
+      },
+      {
+        label: "Clear Items",
+        click() {
+          mainWindow.webContents.send("item:clear");
+        }
+      },
+      {
+        label: "Quit",
+        accelerator: process.platform == "darwin" ? "Command+Q" : "Ctrl+Q",
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
